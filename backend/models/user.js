14d5593bca41717820dc8a534749c150dcc87bf3
Young, Jake <jake_young@dell.com>
2022-06-14 23:01:46 +0100

    Adding User and Password routes

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,45 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
+    name: {
+        type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: false
+    },
+    street: {
+        type: String,
+        required: ''
+    },
+    apartment: {
+        type: String,
+        required: ''
+    },
+    zip: {
+        type: String,
+        required: ''
+    },
+    city: {
+        type: String,
+        required: ''
+    },
+    country: {
+        type: String,
+        required: ''
     }
 })
 
